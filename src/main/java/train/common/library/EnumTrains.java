31be8ce335554ce0b6c000e0974b03cfbcab01ec
Chazi <chaziw898@gmail.com>
2026-03-11 12:33:56 +0000

    Fixes n beep beeps
    SMP part 390 panto
    couldn't get into High Speed loco inventory & seat was borked
    also more Beep Beep sounds

--- src/main/java/train/common/library/EnumTrains.java
+++ src/main/java/train/common/library/EnumTrains.java
@@ -210,7 +210,7 @@ public class EnumTrains {
 				TrainRecord.makeEntry("locoElectricVL10", "Loco Electric VL10", EntityLocoElectricVL10.class, ItemIDs.minecartVL10.item, "electric", 6250, 100, 0, 8, 0, 400, 1.1, 0.956, 0, null, 14, -2.3),
 				TrainRecord.makeEntry("locoElectricBR_E69", "Loco Electric BR_E69", EntityLocoElectricBR_E69.class, ItemIDs.minecartBR_E69.item, "electric", 400, 50, 0, 5, 0, 400, 0.9, 0.946, 0, new String[]{"Green", "Red", "Black", "Grey"}, 18, 0),
 				TrainRecord.makeEntry("locoElectricMineTrain", "Loco Electric Minetrain", EntityLocoElectricMinetrain.class, ItemIDs.minecartLocoMineTrain.item, "electric", 500, 40, 0, 80, 0, 160, 0.5, 0.97, 0, null, 18, -1),
-				TrainRecord.makeEntry("locoElectricSpeedZeroED", "Loco Electric High Speed ZeroED", EntityLocoElectricHighSpeedZeroED.class, ItemIDs.minecartLocoHighSpeedZeroED.item, "electric", 700, 240, 0, 3, 0, 230, 1.4, 0.98, 0, null, 13, -3.4),
+				TrainRecord.makeEntry("locoElectricSpeedZeroED", "Loco Electric High Speed ZeroED", EntityLocoElectricHighSpeedZeroED.class, ItemIDs.minecartLocoHighSpeedZeroED.item, "electric", 700, 240, 0, 3, 0, 230, 1.2, 0.96, 0, null, 13, -3.4),
 				TrainRecord.makeEntry("locoElectricICE1", "Loco Electric ICE 1", EntityLocoElectricICE1.class, ItemIDs.minecartICE1_Loco.item,
 						"electric", 869, 280, 0, 40, 0, 250, 1.5, 0.98, 0, new String[]{"White", "Blue"}, 13, -5),
 				//TrainRecord.makeEntry("locoSpeedGrey",Loco High Speed",EntityLocoElectricNewHighSpeedLoco.class, null,null),
