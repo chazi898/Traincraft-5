c2bd45be20df5d0ef1ba0398981c27b3dafac09c
Chazi <chaziw898@gmail.com>
2026-03-30 01:22:47 +0100

    I swear to fuckingg
    TGV passenger car rider fix

--- src/main/java/train/common/entity/rollingStock/EntityPassengerTGVR1R8Coach.java
+++ src/main/java/train/common/entity/rollingStock/EntityPassengerTGVR1R8Coach.java
@@ -30,8 +30,8 @@ public class EntityPassengerTGVR1R8Coach extends EntityRollingStock implements I
     public void updateRiderPosition() {
         if (riddenByEntity == null) {return;}
         double pitchRads = this.anglePitchClient * Math.PI / 180.0D;
-        double distance = 1.5f;
-        double yOffset = -0.25f;
+        double distance = 0f;
+        double yOffset = -0.2f;
         float rotationCos1 = (float) Math.cos(Math.toRadians(this.renderYaw + 90));
         float rotationSin1 = (float) Math.sin(Math.toRadians((this.renderYaw + 90)));
         if(side.isServer()){
