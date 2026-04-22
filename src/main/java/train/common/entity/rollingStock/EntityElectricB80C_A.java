894e6e728688f18d925897e8f4a076c75bf27024
CBwagen <yugocris3@gmail.com>
2026-04-22 14:36:56 +0200

    Now you B80C me again
    B80C Overhaul part. 1

--- src/main/java/train/common/entity/rollingStock/EntityElectricB80C_A.java
+++ src/main/java/train/common/entity/rollingStock/EntityElectricB80C_A.java
@@ -35,7 +35,7 @@ public class EntityElectricB80C_A extends ElectricTrain {
     public void updateRiderPosition() {
         if (riddenByEntity == null) {return;}
         double pitchRads = this.anglePitchClient * Math.PI / 180.0D;
-        double distance = 2.8;
+        double distance = 2.95;
         double yOffset = -0.1;
         float rotationCos1 = (float) Math.cos(Math.toRadians(this.renderYaw + 90));
         float rotationSin1 = (float) Math.sin(Math.toRadians((this.renderYaw + 90)));
